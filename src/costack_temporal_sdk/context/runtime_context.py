249e605250ca664f24ed559f6a6f18de7cf8f2ec
QianXiaoMoRan9 <wuyi5105131@163.com>
2022-04-20 18:52:13 -0400

    Add lambda decorator

--- src/costack_temporal_sdk/context/runtime_context.py
+++ src/costack_temporal_sdk/context/runtime_context.py
@@ -4,7 +4,8 @@ from costack_temporal_sdk.context.function_context import FunctionContext
 class RuntimeContext:
     def __init__(self, debug):
         self._debug = debug
-
+        self._entry_context = dict()
+        self._exit_context = dict()
         self._steps: List[FunctionContext] = []
 
     @property
@@ -13,6 +14,18 @@ class RuntimeContext:
     @property
     def steps(self):
         return self._steps
+    @property
+    def entry_context(self):
+        return self._entry_context
+    @property
+    def exit_context(self):
+        return self._exit_context
     
     def add_step(self, step):
         self._steps.append(step)
+    
+    def add_entry_context(self, key, context):
+        self._entry_context[key] = context
+    def add_exit_context(self, key, context):
+        self._exit_context[key] = context
+    
--- src/test.py
+++ src/test.py
@@ -1,12 +1,19 @@
 from costack_temporal_sdk.context.runtime_context import RuntimeContext
 import config
 from costack_temporal_sdk.step import step
+from costack_temporal_sdk.lambda_runtime import lambda_runtime
+import os
 
 def func(a, b): 
-    return a + b
+    return a+b
+
+@lambda_runtime
+def handler(event, context): 
+    print(context)
+    step(func, 3, 4)
+    return event
 
 if __name__ == "__main__":
-    config.runtime =  (RuntimeContext(False))
-    
-    print(step(func, 3, 4))
+    os.environ["DEBUG"] = "True"
+    print(handler("event", "context"))
     print(config.runtime)
